524563325162d0a06cc7c8391f82f6a3f2f82c0b
Casey Dwyer <caseydwyer@gmail.com>
2015-05-01 02:35:43 -0700

    Pass BlobStore instance to server. Handle exceptions with 500 error.
    Signed-off-by: Casey Dwyer <caseydwyer@gmail.com>

--- btfupy/server.py
+++ btfupy/server.py
@@ -1,26 +1,29 @@
 import BaseHTTPServer
 import SocketServer
 
-import bs
 
-store = bs.BlobStore()
-
-class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+class BlobRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def do_GET(self):
-        ref = self.path[1:]
-        blob = store.get_blob(ref)
-        if blob is not None:
-            self.response(blob, content_type='application/octet-stream')
-        else:
-            self.response(status=404)
+        try:
+            ref = self.path[1:]
+            blob = self.server.store.get_blob(ref)
+            if blob is not None:
+                self.response(blob, content_type='application/octet-stream')
+            else:
+                self.response(status=404)
+        except Exception, e:
+            self.response(str(e), status=500)
 
     def do_POST(self):
-        if self.path == '/':
-            n = int(self.headers['Content-Length'])
-            self.response(store.put_blob(self.rfile.read(n)))
-        else:
-            self.response(status=404)
+        try:
+            if self.path == '/':
+                n = int(self.headers['Content-Length'])
+                self.response(self.server.store.put_blob(self.rfile.read(n)))
+            else:
+                self.response(status=404)
+        except Exception, e:
+            self.response(str(e), status=500)
 
     def response(self, blob='', status=200, content_type='text/plain'):
         self.send_response(status)
@@ -29,7 +32,14 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(blob)
 
 
-def serve(host, port):
+class BlobServer(SocketServer.TCPServer):
+
+    def __init__(self, store, host, port):
+        SocketServer.TCPServer.__init__(self, (host, port), BlobRequestHandler)
+        self.store = store
+
+
+def serve(store, host, port):
     print 'listening on %s:%d' % (host, port)
-    httpd = SocketServer.TCPServer((host, port), Handler)
+    httpd = BlobServer(store, host, port)
     httpd.serve_forever()
